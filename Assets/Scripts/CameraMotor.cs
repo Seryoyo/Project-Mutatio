8df22acff8389ad97aef6fcd49ea4c07e7144a21
Anh <sharpeipuppynumberonefan@gmail.com>
2025-04-15 14:27:09 -0700

    Created test map

---
+++ Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    public Transform lookAt;
+    // How far can player go before camera follows
+    public float boundX = 0.15f;
+    public float boundY = 0.15f;
+
+    // Late update so we can move camera after player moves w/ FixedUpdate
+    // AKA: avoid small (but annoying) desync
+    private void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+
+        // Check if we're inside the bounds on the X axis
+        float deltaX = lookAt.position.x - transform.position.x;
+        if (deltaX > boundX || deltaX < -boundX)
+        {
+            if (transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+            } else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+        // Check if we're inside the bounds on the Y axis
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
+    }
+
+}
