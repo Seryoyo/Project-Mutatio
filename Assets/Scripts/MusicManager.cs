ea2b4865d702a14ea1942d5dab667067cbb6739a
kenneth <kennethchau@cpp.edu>
2025-05-14 23:27:02 -0700

    updated music manager logic

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MusicManager : MonoBehaviour
-{
+public class MusicManager : MonoBehaviour {
     private static MusicManager Instance;
-    private AudioSource audioSrc;
-    public AudioClip bgMusic;
-    //[SerializeField] private Slider musicSlider; // if we add slider???
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource layerSource;
+    [SerializeField] private AudioSource SFXSource;
 
+    [Header("Clips")]
+    public AudioClip bg;
+    public AudioClip heartbeat;
+    public AudioClip userShoot;
 
     public void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-            audioSrc = GetComponent<AudioSource>();
 
             DontDestroyOnLoad(gameObject);
         }
@@ -26,30 +29,14 @@ public class MusicManager : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start() {
-        if (bgMusic != null) {
-            PlayBgMusic(false, bgMusic);
-        }
-        //musicSlider.onValueChanged.AddListener(delegate { SetVolume(musicSlider.value); }); // if we add slider we can use this
-    }
-
-    public static void SetVolume(float volume) {
-        Instance.audioSrc.volume = volume;
-    }
-
-    public static void PlayBgMusic(bool resetSong, AudioClip audioClip = null) {
-        if (audioClip != null) {
-            Instance.audioSrc.clip = audioClip;
-        } else if (Instance.audioSrc.clip != null) {
-            if (resetSong) {
-                Instance.audioSrc.Stop();
-            }
-            Instance.audioSrc.Play();
-        }
-    }
-
-    public static void PauseBgMusic() {
-        Instance.audioSrc.Pause();
+    private void Start() {
+        musicSource.clip = bg;
+        musicSource.loop = true;
+        musicSource.volume = 0.1f;
+        musicSource.Play();
+        layerSource.clip = heartbeat;
+        layerSource.loop = true;
+        layerSource.volume = 0.2f;
+        layerSource.Play();
     }
 }
